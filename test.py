b64d1cac4d37b054b10894e7ad5613fb733332b4
死老百姓 심의정 <96654161+Trinity-SYT-SECURITY@users.noreply.github.com>
2022-12-28 22:47:28 +0800

    Update test.py

--- test.py
+++ test.py
@@ -19,15 +19,16 @@ def outlook_mail():
     inbox = account.GetDefaultFolder(6)  # 數字6代表收件箱
     # 獲取收件箱下所有郵件
     mails = inbox.Items
-    mails_count = len(mails)  # 邮件数量
-    print("邮件数量：", mails_count)
+    mails_count = len(mails)  # 郵件數量
+    print("郵件數量：", mails_count)
 
     received_dt = datetime.now() - timedelta(days=int(dayy))
     received_dt = received_dt.strftime('%m/%d/%Y %H:%M %p')
     mailss = mails.Restrict("[ReceivedTime] >= '" + received_dt + "'")
     mailss = mails.Restrict("[SenderEmailAddress] = '" + send + "'")
-    # mails = mails.Restrict("[UnRead] = 'True'")過濾未讀取郵件
-    print(len(mailss))
+    
+    #mails = mails.Restrict("[UnRead] = 'True'") 過濾未讀取郵件
+    #print(len(mailss))
 
     try:
         for message in list(mailss):
@@ -75,9 +76,13 @@ if __name__ == '__main__':
     #
     """outputDir = input("你想將檔案儲存在哪個目錄下?請輸入目錄路徑!! (EX: C:\\test\) : ").strip()
     print(outputDir)"""
-    outputDir = r"C:\\Users\\user\\Desktop\\郵件分類器\\outputeven"
+    
+    outputDir = r"C:\\outputeven" #改成自己想把郵件附件存在哪個目錄下!必須確保該目錄存在!!!
+    
     user = input("這邊會刪除剛輸入的目錄下所有檔案，如果不要請輸入 n 如果需要輸入 y :").strip()
+    
     # 這樣搞是因為，每次執行程式若把郵件輸出到相同的目錄下，就會堆疊太多紀錄，如果沒需要這功能，可以把它刪掉
+    
     if user == 'y':
         files = os.listdir(outputDir)
     # Iterate through the list of files and use os.remove() to delete each file
@@ -88,6 +93,7 @@ if __name__ == '__main__':
     elif user == 'n':
         print("接下來你所新生成的檔案會在此目錄下，且會包含上一次生成的檔案\n")
     outlook_mail()
+    
 #output = input("輸入要將郵件內容輸出進去的txt檔案名稱 EX: output")
 #output = output+'.txt'
 #f = open("output.txt", "r+")
